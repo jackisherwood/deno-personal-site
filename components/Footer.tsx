a4dcf0f39f182f1323f9c9db9e1bcdf4d3fe1091
Jack Isherwood <Jisherwood12@gmail.com>
2023-02-18 20:02:16 +0000

    Fix text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class=" py-12 md:flex md:items-center md:justify-between">
           <div class="flex space-x-6 md:order-2"></div>
-          <p class="mt-8 text-base text-gray-400 md:order-1 md:mt-0">
+          <p class="mt-8 text-base text-gray-600 md:order-1 md:mt-0">
             &copy; 2023 Jack Isherwood. All rights reserved.
           </p>
         </div>
